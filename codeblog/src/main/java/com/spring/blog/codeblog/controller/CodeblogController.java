e379e4489a0f70026b6ff0f316ef8c6343dd5af0
Igor Santos Oliveira <iigor315@hotmail.com>
2021-08-12 10:47:44 -0300

    Criação do método que mostra um post especifico pelo id Criacao do do arquivo html postDetails

--- codeblog/src/main/java/com/spring/blog/codeblog/controller/CodeblogController.java
+++ codeblog/src/main/java/com/spring/blog/codeblog/controller/CodeblogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,11 +18,21 @@ public class CodeblogController {
 	@Autowired
 	CodeBlogService codeBlogService;
 	
+	//método que mostra os posts na tela
 	@RequestMapping(value = "/posts", method = RequestMethod.GET)
 	public ModelAndView getPosts() {
 		ModelAndView mv = new ModelAndView("posts");
 		List<Post> posts = codeBlogService.findAll();
 		mv.addObject("posts", posts);
+		return mv;
+	}
+	
+	//método que retorna os detalhes especificos de um determinado post pelo "ID"
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = codeBlogService.findById(id);
+		mv.addObject("post", post);
 		return mv;
 	}
 }
